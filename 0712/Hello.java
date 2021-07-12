59bb454c527bbb2776a73e4cf17c1a6f9b3edf68
dustjd42 <dustjd42@gmail.com>
2021-07-13 00:50:34 +0900

    자바 0713 학습

--- 0712/Hello.java
+++ 0712/Hello.java
@@ -3,3 +3,18 @@ public class Hello {
         System.out.println("Hello World");
     }
 }
+
+/*
+
+VS CODE 이용하여, java 
+
+컴파일: 코드르 컴퓨터 언어로 변환
+빌드: 코드를 실행 파일로 변환
+(빌드의 과정 중 컴파일이 있다.)
+(추가로 링크라는 개념도 발견...)
+
+.java -> (컴파일) -> .class -> (jvm) -> RUN
+
+x86: 32비트 이유는 x86 개열에 CPU는 32비트를 지원하기 때문이다.
+
+*/
