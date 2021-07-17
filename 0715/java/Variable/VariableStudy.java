a4980ec3ee21c99824f6f2410a3820269ec9a351
dustjd42 <dustjd42@gmail.com>
2021-07-18 00:04:37 +0900

    자바의 정석 변수

---
+++ 0715/java/Variable/VariableStudy.java
@@ -0,0 +1,23 @@
+public class VariableStudy {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        String b = "과연 결과는?";
+
+        String c;
+        c = a+b;
+
+        System.out.println(a+b);
+        System.out.print(c);
+
+        //상수
+        final int E_FINAL = 1000;
+        System.out.println(E_FINAL);
+
+        
+        float pi = 3.14f;
+
+
+    }
+}
