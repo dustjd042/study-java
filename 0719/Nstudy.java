db73889e042431f2c83e865f66ce4473d423179a
dustjd42 <dustjd42@gmail.com>
2021-07-20 23:21:51 +0900

    백준

--- 0719/Nstudy.java
+++ 0719/Nstudy.java
@@ -7,7 +7,7 @@ public class Nstudy {
 
         int A = scan.nextInt();
 
-        for (int i = A-1; i > 0; i--){
+        for (int i = A; i > 0; --i){
             System.out.println(i);
         }
     }
---
+++ 0720/std.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class std {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+
+        int[] b = new int[a];
+        int[] c = new int[a];
+        
+        for (int i = 0; i < a; i++){
+            b[i] = scan.nextInt();
+            c[i] = scan.nextInt();
+        }
+
+        for (int i = 0; i < a; i++){
+            System.out.printf("Case #%d: %d \n", i + 1, b[i] + c[i]);
+        }
+    }
+}
