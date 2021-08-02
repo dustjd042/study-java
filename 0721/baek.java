084b6ed6980e98c58860bc8cc52efedf050f0136
dustjd42 <dustjd42@gmail.com>
2021-08-03 00:39:07 +0900

    백준 문제

--- 0721/baek.java
+++ 0721/baek.java
@@ -1,21 +1,39 @@
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.zip.ZipEntry;
 
 public class baek {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
 
-        int a = scan.nextInt();
-        
-        int[] b = new int[a];
+        int[] a = new int[9];
+        int[] b = new int[9];
 
-        for(int i = 0; i < a; i++){
-            b[i] = scan.nextInt();
+        for(int i = 0; i < 9; i++){
+            int q = scan.nextInt();
+            a[i] = q;
+            b[i] = q;
         }
 
+        //b = a;
         Arrays.sort(b);
+    
+        int z = 0;
+        while(true){
+            // System.out.printf("========= \n", z);
+            // System.out.printf("%d \n", z);
+            if(b[8] == a[z]){
+                // System.out.printf("========= \n", z);
+                // System.out.printf("%d \n", b[8]);
+                // System.out.printf("========= \n", z);
+                // System.out.printf("%d \n", z);
+                // System.out.printf("========= \n", z);
+                break;
+            }
+            z++;
+        }
 
-        System.out.printf("%d %d", b[0], b[b.length-1]);
+        System.out.printf("%d\n%d",  b[8], z+1);
     }
 }
